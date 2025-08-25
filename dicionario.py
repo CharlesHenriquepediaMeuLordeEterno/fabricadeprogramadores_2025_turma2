bc350d3131ef9f122bbdce673fd5c8d80c0e98b1
Marcos Vinicius <markosllzs658@gmail.com>
2025-08-25 11:48:08 -0300

    aulas de python
    aula de dicionario

---
+++ dicionario.py
@@ -0,0 +1,16 @@
+tabela = { "Alface": 0.45,
+          "Batata": 1.20,
+          "Tomate": 2.30,
+          "Feijão": 1.50}
+tabela["Cebola"] = 1.20
+print(tabela["Tomate"])
+print(tabela["Batata"])
+print(tabela["Alface"])
+print(tabela["Feijão"])
+print(tabela["Cebola"])
+print("Manga" in tabela)
+print("Uva" in tabela)
+print("Banana" in tabela)
+print("Tomate" in tabela)
+
+print(tabela)
