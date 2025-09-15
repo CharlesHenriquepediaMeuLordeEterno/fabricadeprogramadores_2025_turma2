50062e5d2c3654edc7a86584fdfb16403689325b
Marcos Vinicius <markosllzs658@gmail.com>
2025-09-15 11:47:01 -0300

    Aula de python
    csv

---
+++ main.py
@@ -0,0 +1,15 @@
+import csv
+
+exampleFile = open('frutas.csv')
+exampleReader = csv.reader(exampleFile)
+'''exampleData = list(exampleReader)
+exampleData
+
+print(exampleData[0][0])
+print(exampleData[0][1])
+print(exampleData[0][2])
+print(exampleData[1][1])
+print(exampleData[6][1])'''
+
+for row in exampleReader:
+    print('Row #' + str(exampleReader.line_num) + ' ' + str(row))
